9e1d7a80495cf5328b65e288856ee7f03a613ec5
IshtPrasadSingh <ishtprasadsingh@gmail.com>
2026-05-15 00:16:24 +0530

    DailyQuestions

---
+++ src/question2784.java
@@ -0,0 +1,20 @@
+public class question2784 {
+// checking if an array is good
+
+
+//class Solution {
+//    public boolean isGood(int[] nums) {
+//
+//        // sorting method
+//
+//        Arrays.sort(nums);
+//        int n = nums.length-1;
+//        for(int i=0; i<n; i++){
+//            if(nums[i] != i+1){
+//                return false;
+//            }
+//        }
+//        return nums[n] == n;
+//    }
+//}
+}
