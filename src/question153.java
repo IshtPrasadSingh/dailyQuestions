3dc97e3cca1c06d295d2a842b79d3fb336dbcf6a
IshtPrasadSingh <ishtprasadsingh@gmail.com>
2026-05-17 12:55:39 +0530

    leetcode

---
+++ src/question153.java
@@ -0,0 +1,21 @@
+public class question153 {
+//    class Solution {
+//        public int findMin(int[] nums) {
+//            int left = 0;
+//            int right = nums.length - 1;
+//            while(left< right){
+//                int mid = left + (right - left)/2 ;
+//
+//                //minimum lies on right side
+//                if(nums[mid] > nums[right]){
+//                    left = mid + 1;
+//                }else{
+//                    //minimum lies on the left side including mid
+//                    right = mid;
+//                }
+//            }
+//            return nums[left];
+//
+//        }
+//    }
+}
